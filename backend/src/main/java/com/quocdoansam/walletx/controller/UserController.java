a89e7bea93c65caec88161048013bfccecd868ec
quocdoansam <quocdoansam.me@gmail.com>
2025-04-04 01:51:50 +0700

    Changed logic and endpoints.

--- backend/src/main/java/com/quocdoansam/walletx/controller/UserController.java
+++ backend/src/main/java/com/quocdoansam/walletx/controller/UserController.java
@@ -3,25 +3,32 @@ package com.quocdoansam.walletx.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quocdoansam.walletx.dto.request.UserCreationRequest;
 import com.quocdoansam.walletx.dto.response.ApiResponse;
 import com.quocdoansam.walletx.dto.response.UserResponse;
+import com.quocdoansam.walletx.exception.AppException;
+import com.quocdoansam.walletx.exception.ErrorCode;
 import com.quocdoansam.walletx.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/users")
+@Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping()
+    @PostMapping("/users")
     ApiResponse<UserResponse> create(@RequestBody UserCreationRequest request) {
         UserResponse userResponse = userService.create(request);
         return ApiResponse.<UserResponse>builder().result(userResponse).build();
@@ -29,11 +36,21 @@ public class UserController {
 
     @GetMapping("/{username}")
     public ApiResponse<UserResponse> read(@PathVariable String username) {
+
+        String currentUsername = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+
+        if (!currentUsername.equals(username)) {
+            throw new AppException(ErrorCode.FORBIDDEN);
+        }
+
         UserResponse userResponse = userService.read(username);
         return ApiResponse.<UserResponse>builder().result(userResponse).build();
     }
 
-    @GetMapping()
+    @GetMapping("/users")
     public ApiResponse<List<UserResponse>> readAll() {
         List<UserResponse> users = userService.readAll();
         return ApiResponse.<List<UserResponse>>builder().result(users).build();
