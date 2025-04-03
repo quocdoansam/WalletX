69ef4e2d453a597cc754dd7a08a0b84fc904ff72
quocdoansam <quocdoansam.me@gmail.com>
2025-04-03 20:14:42 +0700

    Created "UserCreationRequest" java file.

---
+++ backend/src/main/java/com/quocdoansam/walletx/dto/request/UserCreationRequest.java
@@ -0,0 +1,20 @@
+package com.quocdoansam.walletx.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserCreationRequest {
+    String username;
+    String password;
+    String fullName;
+    String walletAddress;
+}
