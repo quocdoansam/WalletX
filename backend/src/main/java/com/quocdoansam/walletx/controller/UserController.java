2e6b5563b8b69f9d343614bb054e417008841f08
quocdoansam <quocdoansam.me@gmail.com>
2025-04-04 01:08:31 +0700

    Added some routing.

--- backend/src/main/java/com/quocdoansam/walletx/controller/UserController.java
+++ backend/src/main/java/com/quocdoansam/walletx/controller/UserController.java
@@ -1,6 +1,13 @@
 package com.quocdoansam.walletx.controller;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +17,8 @@ import com.quocdoansam.walletx.dto.response.ApiResponse;
 import com.quocdoansam.walletx.dto.response.UserResponse;
 import com.quocdoansam.walletx.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/users")
@@ -18,12 +26,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/create")
-    @SuppressWarnings("unchecked")
+    @PostMapping()
     ApiResponse<UserResponse> create(@RequestBody UserCreationRequest request) {
-        ApiResponse apiResponse = new ApiResponse<>();
+        UserResponse userResponse = userService.create(request);
+        return ApiResponse.<UserResponse>builder().result(userResponse).build();
+    }
 
-        apiResponse.setResult(userService.create(request));
-        return apiResponse;
+    @GetMapping("/{username}")
+    public ApiResponse<UserResponse> read(@PathVariable String username) {
+        UserResponse userResponse = userService.read(username);
+        return ApiResponse.<UserResponse>builder().result(userResponse).build();
+    }
+
+    @GetMapping()
+    public ApiResponse<List<UserResponse>> readAll() {
+        List<UserResponse> users = userService.readAll();
+        return ApiResponse.<List<UserResponse>>builder().result(users).build();
     }
 }
