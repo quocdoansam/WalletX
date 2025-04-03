50220f7d487929d801bea736dcba6a62dc278fca
quocdoansam <quocdoansam.me@gmail.com>
2025-04-04 01:17:09 +0700

    Nothing change.

--- backend/src/main/java/com/quocdoansam/walletx/controller/UserController.java
+++ backend/src/main/java/com/quocdoansam/walletx/controller/UserController.java
@@ -1,13 +1,8 @@
 package com.quocdoansam.walletx.controller;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
