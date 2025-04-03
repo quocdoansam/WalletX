0c8e97c32fd25405df1e8cb30711592ab5dfc160
quocdoansam <quocdoansam.me@gmail.com>
2025-04-03 20:30:46 +0700

    Created "UserService" java file.

---
+++ backend/src/main/java/com/quocdoansam/walletx/service/UserService.java
@@ -0,0 +1,49 @@
+package com.quocdoansam.walletx.service;
+
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.quocdoansam.walletx.dto.request.UserCreationRequest;
+import com.quocdoansam.walletx.dto.response.UserResponse;
+import com.quocdoansam.walletx.entity.User;
+import com.quocdoansam.walletx.enums.Role;
+import com.quocdoansam.walletx.exception.AppException;
+import com.quocdoansam.walletx.exception.ErrorCode;
+import com.quocdoansam.walletx.mapper.UserMapper;
+import com.quocdoansam.walletx.repository.UserRepository;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    UserMapper userMapper;
+
+    public UserResponse create(UserCreationRequest request) {
+        if (userRepository.isUserExistedByUsername(request.getUsername())) {
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+
+        User user = userMapper.toCreationUser(request);
+
+        // Start the password encryption
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        // Add role
+        HashSet<String> roles = new HashSet<>();
+        roles.add(Role.USER.name());
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+}
