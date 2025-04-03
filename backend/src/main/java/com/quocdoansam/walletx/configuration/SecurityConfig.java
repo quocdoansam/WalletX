fc1da06626a768ccbf31b4fac59449f8f9c5c38b
quocdoansam <quocdoansam.me@gmail.com>
2025-04-03 22:26:01 +0700

    Added jwtDecoder method and modified some logic in "securityFilterChain".

--- backend/src/main/java/com/quocdoansam/walletx/configuration/SecurityConfig.java
+++ backend/src/main/java/com/quocdoansam/walletx/configuration/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.quocdoansam.walletx.configuration;
 
+import java.security.KeyStore.SecretKeyEntry;
+
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.AccessLevel;
@@ -19,7 +27,7 @@ import lombok.experimental.FieldDefaults;
 @EnableMethodSecurity
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SecurityConfig {
-    final String[] PUBLIC_ENDPOINTS = { "/users" };
+    final String[] PUBLIC_ENDPOINTS = { "auth/token", "auth/introspect", "/users/create"};
 
     @Value("${jwt.signerKey}")
     String signerKey;
@@ -29,14 +37,24 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests(request -> request
                 .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
                 .anyRequest().authenticated());
-        
-        httpSecurity.csrf(httpSecurityCsrfConfigurner -> httpSecurityCsrfConfigurner.disable());
+
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
+        httpSecurity.csrf(csrf -> csrf.disable());
 
         return httpSecurity.build();
     }
 
     @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HmacSHA512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
+
+    @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
     }
 }
